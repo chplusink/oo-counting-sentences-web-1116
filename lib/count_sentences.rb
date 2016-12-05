214855dbf9a1a56c19f9875dcf02cd4965c1ea01
VIC LAM <viclam@VICs-MacBook-Pro.local>
2016-12-05 14:12:15 -0500

    sentences and stuff

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,20 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.chars[-1] == '.'
   end
 
   def question?
-
+    self.chars[-1] == '?'
   end
 
   def exclamation?
-
+    self.chars[-1] == '!'
   end
 
   def count_sentences
-
+    self.split(' ').select do |word|
+      word.chars[-1] == '.' || word.chars[-1] == '!' || word.chars[-1] == '?'
+    end.length
   end
 end
